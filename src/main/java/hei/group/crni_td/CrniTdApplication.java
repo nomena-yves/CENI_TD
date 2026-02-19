8ea93b4367fb8e926cef3f253bf025d33459729e
nomena <hei.nomenjanahary@gmail.com>
2026-02-19 16:26:22 +0300

    first commit

---
+++ src/main/java/hei/group/crni_td/CrniTdApplication.java
@@ -0,0 +1,13 @@
+package hei.group.crni_td;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrniTdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrniTdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/group/crni_td/CrniTdApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.group.crni_td;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrniTdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
